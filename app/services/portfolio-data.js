cd6ffa28645f7e541d319def2ee74b930b8db0b9
eccegordo <eccegordo@gmail.com>
2017-06-26 02:56:06 -0400

    more portofolio items for index page

--- app/services/portfolio-data.js
+++ app/services/portfolio-data.js
@@ -3,23 +3,60 @@ import Ember from 'ember';
 export default Ember.Service.extend({
 
   items: Ember.A([
+    // { 
+    //   id: 'dellemc',
+    //   aboutPath: '../portfolio/dellemc/about.md',
+    //   imagePath: '../portfolio/dellemc/img1.jpg'  
+    // },
+    { 
+      id: 'isilon',
+      aboutPath: '../portfolio/isilon/about.md',
+      imagePath: '../portfolio/isilon/img1.jpg'  
+    },
     { 
       id: 'atthespine.org',
       aboutPath: '../portfolio/atthespine.org/about.md',
       imagePath: '../portfolio/atthespine.org/img1.jpg'  
     },
+    { 
+      id: 'colinspring.com',
+      aboutPath: '../portfolio/colinspring.com/about.md',
+      imagePath: '../portfolio/colinspring.com/img1.jpg'  
+    },
+    { 
+      id: 'georgefearingforcongress.com',
+      aboutPath: '../portfolio/georgefearingforcongress.com/about.md',
+      imagePath: '../portfolio/georgefearingforcongress.com/img1.jpg'  
+    },
+    { 
+      id: 'globalseepej.org',
+      aboutPath: '../portfolio/globalseepej.org/about.md',
+      imagePath: '../portfolio/globalseepej.org/img1.jpg'  
+    },
+    { 
+      id: 'lockdownnetworks.com',
+      aboutPath: '../portfolio/lockdownnetworks.com/about.md',
+      imagePath: '../portfolio/lockdownnetworks.com/img1.jpg'  
+    },
+    { 
+      id: 'murdersilence.com',
+      aboutPath: '../portfolio/murdersilence.com/about.md',
+      imagePath: '../portfolio/murdersilence.com/img1.jpg'  
+    },
+    { 
+      id: 'tapintupdapp',
+      aboutPath: '../portfolio/tapintupdapp/about.md',
+      imagePath: '../portfolio/tapintupdapp/img1.jpg'  
+    },
+    // { 
+    //   id: 'thomasenglish',
+    //   aboutPath: '../portfolio/thomasenglish/about.md',
+    //   imagePath: '../portfolio/thomasenglish/img1.jpg'  
+    // },
+    { 
+      id: 'trimergence.com',
+      aboutPath: '../portfolio/trimergence.com/about.md',
+      imagePath: '../portfolio/trimergence.com/img1.jpg'  
+    },
   ])
-  // imagePaths: [
-  //   "../portfolio/atthespine.org/img1.jpg",
-  //   "../portfolio/colinspring.com/img1.jpg",
-  //   // "../portfolio/dellemc/img1.jpg",
-  //   "../portfolio/georgefearingforcongress.com/img1.jpg",
-  //   "../portfolio/globalseepej.org/img1.jpg",
-  //   "../portfolio/isilon/img1.jpg",
-  //   "../portfolio/lockdownnetworks.com/img1.jpg",
-  //   "../portfolio/murdersilence.com/img1.jpg",
-  //   "../portfolio/tapintupdapp/img1.jpg",
-  //   "../portfolio/thomasenglish/img1.jpg",
-  //   "../portfolio/trimergence.com/img1.jpg",
-  // ],
 });
