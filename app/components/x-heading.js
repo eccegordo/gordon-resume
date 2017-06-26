e5a8cfeef95e3d41ed6d9e873ed603d4974ddaa9
eccegordo <eccegordo@gmail.com>
2017-06-26 00:33:25 -0400

    accent color

--- app/components/x-heading.js
+++ app/components/x-heading.js
@@ -4,5 +4,7 @@ import TypographySupport from '../mixins/typography-support';
 const { Component } = Ember;
 
 export default Component.extend(TypographySupport, {
-  tagName: 'span'
+  tagName: 'span',
+  classNameBindings: ['accent:accent-color'],
+  accent: true
 });
