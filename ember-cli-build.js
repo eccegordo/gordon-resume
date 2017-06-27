b32a8b74068d73a5f3b510a9113f077e77cf541c
eccegordo <eccegordo@gmail.com>
2017-06-27 00:31:28 -0400

    exclude portfolio files from asset fingerprinting

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+   fingerprint: {
+     exclude: ['portfolioFiles/'],
+     prepend: ''
+   }
   });
 
   // Use `app.import` to add additional libraries to the generated
